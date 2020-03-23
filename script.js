8f45bb21e155d77d9ace6eede30692a1951090dc
Anthony Osiol <funnynoun@gmail.com>
2020-03-23 19:18:54 -0400

    refined api code

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
-// $(document).ready(function() {
- 
-function buildQueryURL() {
-  
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" 
 
-var queryParams = { "api-key" : "&appid=75c9c3885f537245e3ffc95d9728c4b1"};
+// var authKey = "&appid=75c9c3885f537245e3ffc95d9728c4b1";
+var authKey = "75c9c3885f537245e3ffc95d9728c4b1";
 
-// Getting location input from user
-queryParams.q = $('#city-search')
-   .val()
-   .trim();
+function buildWeatherURL(location, apiKey){
+  return "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + apiKey;
+}
 
+function buildForecastURL(location, apiKey){
+  return "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&units=imperial&appid=" + apiKey;
+}
 
-// Logging URL for troubleshooting
- console.log("----------------------\nURL: " + queryURL + "\n--------------");
- console.log(queryURL + $.param(queryParams));
- return queryURL + $.param(queryParams);  
-}   
+function buildUVURL(apiKey, coordLat, coordLon){
+  return "https://api.openweathermap.org/data/2.5/uvi?" + "&lat=" + coordLat + "&lon=" + coordLon + "&appid=" + apiKey;
+}
 
+var weatherData, uvData, fiveDayData;
 
-
-
-$('#search-btn').on('click', function(event) {
+$('#search-btn').on('click', function (event) {
 
   event.preventDefault();
+  var location = $('#search-input').val();
+  $('#search-input').val("");
 
-  clear();
+  // Calling series of 3 api in promise chain
+  callAPI(buildForecastURL(location, authKey))
+  .then(function(resp){
+    console.log("five day output", resp);
+    fiveDayData = resp;
+  })
+  callAPI(buildWeatherURL(location, authKey))
+  .then(function (resp){
+    console.log("weather output", resp);
+    weatherData = resp;
+    var coordLon = resp.coord.lon;
+    var coordLat = resp.coord.lat;
+    callAPI(buildUVURL(authKey, coordLat, coordLon))
+    .then(function(resp){
+      console.log("uv output", resp);
+      uvData = resp;   
+    })
+  });
+ });
 
-  var queryURL = buildQueryURL();
-
-   
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(updatePage);
-      
-      });
-      // console.log(response);
-    
+function callAPI(URL) {
+  return $.ajax({
+    url: URL,
+    method: "GET"
+  });
+}
 
 
-
-
-
-
-
-
-
-
-// });
