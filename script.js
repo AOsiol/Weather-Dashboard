7c90db04edc1259055f9b0885d649228f9a7e8ac
Anthony Osiol <funnynoun@gmail.com>
2020-04-03 20:09:05 -0400

    reworked api call and appending data

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-
-// var authKey = "&appid=75c9c3885f537245e3ffc95d9728c4b1";
 var authKey = "75c9c3885f537245e3ffc95d9728c4b1";
 
-function buildWeatherURL(location, apiKey){
+function buildWeatherURL(location, apiKey) {
   return "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + apiKey;
 }
 
-function buildForecastURL(location, apiKey){
+function buildForecastURL(location, apiKey) {
   return "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&units=imperial&appid=" + apiKey;
 }
 
-function buildUVURL(apiKey, coordLat, coordLon){
+function buildUVURL(apiKey, coordLat, coordLon) {
   return "https://api.openweathermap.org/data/2.5/uvi?" + "&lat=" + coordLat + "&lon=" + coordLon + "&appid=" + apiKey;
 }
 
-var weatherData, uvData, fiveDayData;
+var fiveDayData, weatherData, uvData;
 
 $('#search-btn').on('click', function (event) {
 
@@ -24,29 +22,74 @@ $('#search-btn').on('click', function (event) {
 
   // Calling series of 3 api in promise chain
   callAPI(buildForecastURL(location, authKey))
-  .then(function(resp){
-    console.log("five day output", resp);
-    fiveDayData = resp;
-  })
-  callAPI(buildWeatherURL(location, authKey))
-  .then(function (resp){
-    console.log("weather output", resp);
-    weatherData = resp;
-    var coordLon = resp.coord.lon;
-    var coordLat = resp.coord.lat;
-    callAPI(buildUVURL(authKey, coordLat, coordLon))
-    .then(function(resp){
-      console.log("uv output", resp);
-      uvData = resp;   
+    .then(function (resp) {
+      console.log("five day output", resp);
+      fiveDayData = resp;
     })
-  });
- });
+  callAPI(buildWeatherURL(location, authKey))
+    .then(function (resp) {
+      console.log("weather output", resp);
+      weatherData = resp;
+      var coordLon = resp.coord.lon;
+      var coordLat = resp.coord.lat;
+      callAPI(buildUVURL(authKey, coordLat, coordLon))
+        .then(function (resp) {
+          console.log("uv output", resp);
+          uvData = resp;
+          bindDataToDocument();
+        })
+    });
+});
 
 function callAPI(URL) {
-  return $.ajax({
+  return $.ajax({                                         
     url: URL,
     method: "GET"
-  });
+  })
+}
+
+function bindDataToDocument(){
+
+  for (let i = 0; i < fiveDayData.list.length; i++) {
+    if(fiveDayData.list[i].dt_txt.indexOf("12:00:00") !== -1){
+    // DOM Forecast
+      var forecastDate = (fiveDayData.list[i].dt_txt);
+      $('#forecast').append(fiveDayData.list[i].dt_txt);
+      var forecastIcon = (fiveDayData.list[i].weather[0].icon);
+      var forecastTemp = (fiveDayData.list[i].main.temp);
+      var forecastHum = (fiveDayData.list[i].main.humidity); 
+    }
+  }
+   // DOM Current Weather
+var iconcode = weatherData.weather[0].icon;
+var iconurl = "http://openweathermap.org/img/w/"+ iconcode + ".png";
+var sec = uvData.date;
+var date = sec*1000;
+// var timestr = date.toLocaleTimeString();
+
+  $('#location').append(weatherData.name, date);
+  $('#wicon').attr('src', iconurl);
+  $('#temp').append('temperature: '+weatherData.main.temp);
+  $('#humidity').append('humidity: '+weatherData.main.humidity);
+  $('#windspeed').append('wind speed: '+weatherData.wind.speed);
+  $('#uv-index').append('uv index: '+uvData.value);
+    
 }
 
 
+// $('#location').append(weatherData.name, uvData.date_iso);
+
+
+        
+
+
+    
+
+
+    
+
+    
+  
+    
+
+  
