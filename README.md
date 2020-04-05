c01b71631a1312552cd0b745c903c37e9e255b3c
Anthony Osiol <funnynoun@gmail.com>
2020-04-05 17:36:19 -0400

    git status clear

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Weather-Dashboard
+Weather Dashboard
+
+This is an interactive webpage for the user to get weather data.
+
+The user inputs a location in the search box and the page returns the name of the location, date, temperature, humidity, windspeed and uv index. This also returns a five day forecast that includes the date, icon for the weather,temperature and humditiy.
+
+This project requires an ajax call and JSON return. The desired data had to be isolated from the returning data from the api and displayed using javascript to modify the DOM. There were a few challenges with this api call, especially concerning having to make a promise chain. The call to get the info for the uv index was dependent on the coordinates returned from getting the local weather.
+Also displayed are two differnt approaches to modifying the html with javascript. One example is inserting the data within a template iteral in the js file and the other is directing the info individually using indepentent ids.
--- script.js
+++ script.js
@@ -1,95 +1,106 @@
 var authKey = "75c9c3885f537245e3ffc95d9728c4b1";
 
 function buildWeatherURL(location, apiKey) {
-  return "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + apiKey;
+  return (
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&units=imperial&appid=" +
+    apiKey
+  );
 }
 
 function buildForecastURL(location, apiKey) {
-  return "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&units=imperial&appid=" + apiKey;
+  return (
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    location +
+    "&units=imperial&appid=" +
+    apiKey
+  );
 }
 
 function buildUVURL(apiKey, coordLat, coordLon) {
-  return "https://api.openweathermap.org/data/2.5/uvi?" + "&lat=" + coordLat + "&lon=" + coordLon + "&appid=" + apiKey;
+  return (
+    "https://api.openweathermap.org/data/2.5/uvi?" +
+    "&lat=" +
+    coordLat +
+    "&lon=" +
+    coordLon +
+    "&appid=" +
+    apiKey
+  );
+}
+
+function callAPI(URL) {
+  return $.ajax({
+    url: URL,
+    method: "GET",
+  });
 }
 
 var fiveDayData, weatherData, uvData;
 
-$('#search-btn').on('click', function (event) {
-
+$("#search-btn").on("click", function (event) {
   event.preventDefault();
-  var location = $('#search-input').val();
-  $('#search-input').val("");
+  var location = $("#search-input").val();
+  $("#search-input").val("");
 
   // Calling series of 3 api in promise chain
-  callAPI(buildForecastURL(location, authKey))
-    .then(function (resp) {
-      console.log("five day output", resp);
-      fiveDayData = resp;
-    })
-  callAPI(buildWeatherURL(location, authKey))
-    .then(function (resp) {
+  callAPI(buildForecastURL(location, authKey)).then(function (resp) {
+    console.log("five day output", resp);
+    fiveDayData = resp;
+    callAPI(buildWeatherURL(location, authKey)).then(function (resp) {
       console.log("weather output", resp);
       weatherData = resp;
       var coordLon = resp.coord.lon;
       var coordLat = resp.coord.lat;
-      callAPI(buildUVURL(authKey, coordLat, coordLon))
-        .then(function (resp) {
-          console.log("uv output", resp);
-          uvData = resp;
-          bindDataToDocument();
-        })
+      callAPI(buildUVURL(authKey, coordLat, coordLon)).then(function (resp) {
+        console.log("uv output", resp);
+        uvData = resp;
+        bindDataToDocument();
+      });
     });
+  });
 });
 
-function callAPI(URL) {
-  return $.ajax({                                         
-    url: URL,
-    method: "GET"
-  })
-}
-
-function bindDataToDocument(){
-
+function bindDataToDocument() {
   for (let i = 0; i < fiveDayData.list.length; i++) {
-    if(fiveDayData.list[i].dt_txt.indexOf("12:00:00") !== -1){
-    // DOM Forecast
-      var forecastDate = (fiveDayData.list[i].dt_txt);
-      $('#forecast').append(fiveDayData.list[i].dt_txt);
-      var forecastIcon = (fiveDayData.list[i].weather[0].icon);
-      var forecastTemp = (fiveDayData.list[i].main.temp);
-      var forecastHum = (fiveDayData.list[i].main.humidity); 
+    if (fiveDayData.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+      // DOM Forecast
+      var forecastIcon = fiveDayData.list[i].weather[0].icon;
+      var forecastIconUrl =
+        "http://openweathermap.org/img/w/" + forecastIcon + ".png";
+      var forecastDate = fiveDayData.list[i].dt_txt;
+      var forecastTemp = fiveDayData.list[i].main.temp;
+      var forecastHum = fiveDayData.list[i].main.humidity;
+
+      $("#forecast").append(
+        `
+       <div class="card" id="forecast-card">
+       <div class="card-header">
+       ${forecastDate}
+       </div
+          <div class="card-body">
+             <h5 class="card-title">Temp: ${forecastTemp}</h5>
+             <img id="forecast-icon" src="${forecastIconUrl}" alt=""> 
+             <p class="card-text">Humidity: ${forecastHum}</p> 
+          </div>
+        </div>
+       <br>
+        `
+      );
     }
   }
-   // DOM Current Weather
-var iconcode = weatherData.weather[0].icon;
-var iconurl = "http://openweathermap.org/img/w/"+ iconcode + ".png";
-var sec = uvData.date;
-var date = sec*1000;
-// var timestr = date.toLocaleTimeString();
 
-  $('#location').append(weatherData.name, date);
-  $('#wicon').attr('src', iconurl);
-  $('#temp').append('temperature: '+weatherData.main.temp);
-  $('#humidity').append('humidity: '+weatherData.main.humidity);
-  $('#windspeed').append('wind speed: '+weatherData.wind.speed);
-  $('#uv-index').append('uv index: '+uvData.value);
-    
+  // DOM Current Weather
+  var iconCode = weatherData.weather[0].icon;
+  var iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+
+  $("#location").append(
+    weatherData.name + " " + new Date().toLocaleDateString()
+  );
+  $("#wicon").attr("src", iconUrl);
+  $("#temp").append("temperature: " + weatherData.main.temp);
+  $("#humidity").append("humidity: " + weatherData.main.humidity);
+  $("#windspeed").append("wind speed: " + weatherData.wind.speed);
+  $("#uv-index").append("uv index: " + uvData.value);
 }
-
-
-// $('#location').append(weatherData.name, uvData.date_iso);
-
-
-        
-
-
-    
-
-
-    
-
-    
-  
-    
-
-  
